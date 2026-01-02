3a0987986e91a21d6186c438b7feeba172ad8ca6
jjudykim <jjudykim0@gmail.com>
2026-01-02 18:22:07 +0900

    Boss Transition State 구현 중
    - Boss의 피가 50% 이하로 도달한다면 Phase 전환되도록 구현
    - Transition State 구현 중
    - HUD 반영 예정
    
    TODO : 애니메이션 먼저 작업해놓기

---
+++ Assets/10. Scripts/09. UI/BossHpBarUI.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class BossHpBarUI : MonoBehaviour
+{
+    [SerializeField] private Boss boss;
+    [SerializeField] private RectTransform hpBarRect;
+    
+    [Header("Padding")]
+    [SerializeField] private float leftPadding = 10f;
+    [SerializeField] private float rightPaddingMax = 10f;
+
+    private float parentWidth;
+
+    private void Awake()
+    {
+        RectTransform parent = hpBarRect.parent as RectTransform;
+        parentWidth = parent.rect.width;
+    }
+
+    private void Update()
+    {
+        float bossHp = boss.Stat.CurrentHp / boss.Stat.MaxHp;
+        SetHPRatio(bossHp);
+    }
+
+    public void SetHPRatio(float ratio)
+    {
+        ratio = Mathf.Clamp01(ratio);
+
+        float right = Mathf.Lerp(parentWidth - leftPadding, rightPaddingMax, ratio);
+
+        hpBarRect.offsetMin = new Vector2(leftPadding, hpBarRect.offsetMin.y);
+        hpBarRect.offsetMax = new Vector2(-right, hpBarRect.offsetMax.y);
+    }
+}
